b4b4d7d3130d9006a5e32c6225fea580956b63b9
yeticitraaprilianti <92861304+yeticitraaprilianti@users.noreply.github.com>
2022-02-15 08:06:28 +0700

    four commit

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,9 @@
-function Logo () {
-    return (
-        <div className="logo text-white text-2xl text-center uppercase font-semibold ">Yecacitra</div>
-    )
+function Logo() {
+	return (
+		<div className="logo text-white text-2xl text-center uppercase font-semibold tracking-widest">
+			Yecaa
+		</div>
+	);
 }
+
+export default Logo;
